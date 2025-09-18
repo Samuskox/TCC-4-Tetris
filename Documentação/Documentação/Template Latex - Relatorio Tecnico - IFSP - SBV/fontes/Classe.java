df06ad91bb6b7bc25c46f5619459eb3f55df4dff
Samuel Oliveira Lopes <samuolopes@gmail.com>
2025-09-18 19:58:18 -0300

    001-commit-Documentation
    update:
    godot and java
    
    adding:
    subsubsection: libgdx, raylib and pygame

---
+++ Documentação/Documentação/Template Latex - Relatorio Tecnico - IFSP - SBV/fontes/Classe.java
@@ -0,0 +1,24 @@
+public class Classe{
+    public static void main(String[] args) {
+        String variavel;
+        int num;
+
+
+        public String getVariavel(){
+            return variavel;
+        }
+
+        public void setVariavel(String variavel){
+            this.variavel = variavel;
+        }
+
+        public int getNum(){
+            return num;
+        }
+
+        public void setNum(){
+            this.num = num;
+        }
+
+    }
+}
