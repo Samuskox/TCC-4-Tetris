fb9e9355c7d4459b8686e75fd8d82d8918b6dac6
Samuel Oliveira Lopes <samuolopes@gmail.com>
2026-02-03 01:15:08 -0300

    001-commit-godot
    implement lock delay

--- tetrisgodot/Scenes/piece.gd
+++ tetrisgodot/Scenes/piece.gd
@@ -119,15 +119,14 @@ func move(direction: Vector2):
 func fall():
 	print("checkfall: ", checkFall)
 	if not move(Vector2(0,1)):
-		if checkFall == 1:
+		checkFall += 1
+		if checkFall >= 2:
 			lockPiece(self)
+			main_script.cleanLine()
+			main_script.queue_redraw()
 			main_script.spawnPiece()
+			queue_free()
 			checkFall = 0
-		else:
-			pass
-	else:
-		gridPosition.y += 1
-		checkFall = 0
 	
 	#if isValidmove(fall):
 		#gridPosition.y += 1;
